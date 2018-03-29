7b2f4e5f03f9575ba159c8a1aa7cc926f6873307
Max <mbremer@richard-mueller.de>
2018-03-29 11:11:39 +0200

    add title

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
     }
     ?>
 
+    <title>Blog</title>
     <meta charset="<?php bloginfo('charset'); ?>">
 	<link href="<?PHP echo get_template_directory_uri(); ?>/assets/img/icons/favicon.ico" rel="shortcut icon">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
